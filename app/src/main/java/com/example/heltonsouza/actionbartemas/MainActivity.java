a63b87a30d31007b542f958f048379baacc53c1f
Heltonss <Helton@heltonsouza.com.br>
2017-11-20 21:21:55 -0200

    Adicionado toolbar

--- app/src/main/java/com/example/heltonsouza/actionbartemas/MainActivity.java
+++ app/src/main/java/com/example/heltonsouza/actionbartemas/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.heltonsouza.actionbartemas;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -14,12 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Hi");
     }
 
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         //infla o menu com os botoes da action bar
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     private void toast(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
