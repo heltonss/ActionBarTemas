480b73df8638a5942ddab127f45a9787cfb51ee4
Heltonss <Helton@heltonsouza.com.br>
2017-11-20 20:53:53 -0200

    first commit

---
+++ app/src/main/java/com/example/heltonsouza/actionbartemas/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.heltonsouza.actionbartemas;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Hi");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        //infla o menu com os botoes da action bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            toast("Clicou no Search!");
+            return true;
+        } else if (id == R.id.action_refresh) {
+            toast("Clicou no Refresh!");
+            return true;
+        } else if (id == R.id.action_share) {
+            toast("Clicou no Settings!");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
